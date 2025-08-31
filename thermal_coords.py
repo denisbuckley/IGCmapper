4cc47c8e4ff43744cc8af64f16551c6e66fe5b35
denisbuckley <denisbuckleycompro@gmail.com>
2025-08-31 11:50:58 +0930

    parse multiple igc files from folder, return dataframes of coordinates and strengths, filtered from user inputs

--- thermal_coords.py
+++ thermal_coords.py
@@ -5,6 +5,7 @@
 # prompts the user for analysis parameters, providing default values.
 # detailed description at end of script
 
+
 # DataFrame with strength saved to 'consolidated_thermal_data.csv'
 # DataFrame with only coordinates saved to 'consolidated_thermal_coords.csv'
 
